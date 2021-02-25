481b814d21531e548c6810b6e687390f9d053043
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2021-02-25 13:04:46 -0500

    Lista de ollas comunes

--- 1_CSV_DarFormatoXY/main.py
+++ 1_CSV_DarFormatoXY/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import csv
+import codecs
 from datetime import datetime
 from geojson import Feature, FeatureCollection, Point
 ''' 
@@ -15,32 +15,85 @@ nameCSV = "MSI_{2}{1}{0}_{3}{4}{5}".format(nowTime.year,nowTime.month,nowTime.da
 # 2. Se abre el CSV para escribir
 with open('CSV/{}.csv'.format(nameCSV),'w') as writerCSV:
 	# 3. Se lee el CSV para escribir en el primer CSV
-	with open('SOURCE/20200609184622.csv') as readerCSV:
+	with open('SOURCE/listaollas.csv') as readerCSV:
 		rowCSV = csv.DictReader(readerCSV)
 		writer = csv.DictWriter(writerCSV,fieldnames=rowCSV.fieldnames)
 		writer.writeheader()
+		
 		for item in rowCSV:
-			item['Longitude'] = item['Longitude'].replace("West:","-")
-			item['Latitude']  = item['Latitude'].replace("South:","-")
+			#item['Longitude'] = item['Longitude'].replace("West:","-")
+			#item['Latitude']  = item['Latitude'].replace("South:","-")
+			item['longitud'] = item['longitud'].replace(",",".")
+			item['latitud']  = item['latitud'].replace(",",".")
 			writer.writerow(item)
+		
 
 features = []
-with open('CSV/{}.csv'.format(nameCSV)) as csvFile:
+with codecs.open('CSV/{}.csv'.format(nameCSV),encoding='utf-8',errors='ignore') as csvFile:
     rowCSV = csv.DictReader(csvFile)
     for item in rowCSV:
-    	latitude, longitude = map(float,(item['Latitude'], item['Longitude']))
+    	print(item['latitud'])
+    	print(item['longitud'])
+    	latitude, longitude = map(float,(item['latitud'], item['longitud']))
     	features.append(
             Feature(
                 geometry = Point((longitude,latitude)),
                 properties = {
-                    'N':item['\xef\xbb\xbfNo.'],
-			    	'Device Name':item['Device Name'],
-			    	'Receiving Time':item['Receiving Time'],
-			    	'GPS Time':item['GPS Time'],
-			    	'Speed':item['Speed'],
-			    	'Direction':item['Direction'],
-			    	'BaseStation ID':item['BaseStation ID'],
-			    	'Channel ID':item['Channel ID']
+                    'id': item['id'],
+			    	'correo': item['correo'],
+			    	'distrito': item['distrito'],
+			    	'zona': item['zona'],
+			    	'aahh': item['aahh'],
+			    	'ubicacion': item['ubicacion'],
+			    	'agua': item['agua'],
+			    	'luz': item['luz'],
+			    	'nombre_olla': item['nombre_olla'],
+			    	'nombre_contacto': item['nombre_contacto'],
+			    	'cargo_contacto': item['cargo_contacto'],
+			    	'celular_contacto': item['celular_contacto'],
+			    	'presencia_muni': item['presencia_muni'],
+			    	'insumos': item['insumos'],
+			    	'org_ayuda': item['org_ayuda'],
+			    	'instrumentos': item['instrumentos'],
+			    	'dias_atencion': item['dias_atencion'],
+			    	'comidas_dia': item['comidas_dia'],
+			    	'raciones': item['raciones'],
+			    	'precio_racion': item['precio_racion'],
+			    	'raciones_pagadas': item['raciones_pagadas'],
+			    	'zonas_beneficiadas': item['zonas_beneficiadas'],
+			    	'familias_beneficiadas': item['familias_beneficiadas'],
+			    	'ninos_beneficiadas': item['ninos_beneficiadas'],
+			    	'adultos_beneficiadas': item['adultos_beneficiadas'],
+			    	'disca_beneficiadas': item['disca_beneficiadas'],
+			    	'emba_beneficiadas': item['emba_beneficiadas'],
+			    	'enfercro_beneficiadas':item['enfercro_beneficiadas'],
+			    	'observaciones': item['observaciones'],
+			    	'total_beneficiadas': item['total_beneficiadas'],
+			    	'foto':item['foto'],
+			    	'latitud':item['latitud'],
+			    	'longitud':item['longitud'],
+			    	'estado':item['estado'],
+			    	'padrones':item['padrones'],
+			    	'comedorpopular':item['comedorpopular'],
+			    	'comite':item['comite'],
+			    	'nombre_contacto_secundario':item['nombre_contacto_secundario'],
+			    	'cargo_contacto_secundario':item['cargo_contacto_secundario'],
+			    	'celular_contacto_secundario':item['celular_contacto_secundario'],
+			    	'tipo_espacio':item['tipo_espacio'],
+			    	'estado_espacio':item['estado_espacio'],
+			    	'combustible':item['combustible'],
+			    	'techo':item['techo'],
+			    	'lavado':item['lavado'],
+			    	'higiene':item['higiene'],
+			    	'huerto':item['huerto'],
+			    	'espacio_huerto':item['espacio_huerto'],
+			    	'liderazgo':item['liderazgo'],
+			    	'inocuidad':item['inocuidad'],
+			    	'protocolos':item['protocolos'],
+			    	'extranjera':item['extranjera'],
+			    	'otro_cap':item['otro_cap'],
+			    	'created_at':item['created_at'],
+			    	'updated_at':item['updated_at']
                 }
             )
         )
